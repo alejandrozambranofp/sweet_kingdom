0bc96399d0d9f93df3a524499f83d5f0e2516b66
Alejandro Zambrano <alejandrozambranofp@ibf.cat>
2025-12-02 19:20:46 +0100

    parte_del_header_hecha

--- app/views/header.php
+++ app/views/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/paginaWeb_projecte_ZambranoAlejandro/css/style.css">
+    
 </head>
 <body>
     <header class="header">
@@ -27,17 +28,60 @@
                     </form>
                 </div>
             </nav>
-            <nav>
+            <nav class="iconos_usuario_carrito">
                 <div class="logo_carrito">
-                <a href="#">
-                    <img class="header-user-icon" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/icono_usuario.png"
-                        alt="icono usuario">
-                </a>
-                <p>Aqui ira el carrito</p>
+                    <a href="#">
+                        <img class="header-carrito-icon" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/icono_usuario.png"
+                            alt="carrito"
+                            width="30px"
+                            height="auto">
+                    </a>
+                    <a href="#">
+                        <img class="header-carrito-icon" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/carrito.png"
+                            alt="carrito"
+                            width="30px"
+                            height="auto">
+                    </a>
                 </div>
             </nav>
         </div>
-        
+        <div class="header_bottom">
+            <div>
+                <a href="#"><b><p>Top postres</p></b></a>
+            </div>
+            <div>
+                <a href="#"><b><p>Nuevo</p></b></a>
+            </div>
+            <div>
+                <a href="#"><b><p>Tartas</p></b></a>
+            </div>
+            <div>
+                <a href="#"><b><p>Cupcakes</p></b></a>
+            </div>
+            <div>
+                <a href="#"><b><p>Franquicias</p></b></a>
+            </div>
+            <div>
+                <a href="#"><b><p>Ofertas</p></b></a>
+            </div>
+        </div>
+        <div class="info">
+            <div class="entrega">
+                <img class="camion_entrega" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/camion_entrega.png"
+                    alt="camion entrega">
+                <p>Entrega gratuita a partir de 20€</p>
+            </div>
+            <div class="entrega">
+                <img class="camion_entrega" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/icono_regalo.png"
+                    alt="camion entrega">
+                <p>Entrega gratuita a partir de 20€</p>
+            </div>
+            <div class="entrega">
+                <img class="camion_entrega" src="/paginaWeb_projecte_ZambranoAlejandro/assets/imagenes/ucono_exclusivo.png"
+                    alt="camion entrega">
+                <p>Entrega gratuita a partir de 20€</p>
+            </div>
+        </div>
     </header>
 </body>
 </html>
